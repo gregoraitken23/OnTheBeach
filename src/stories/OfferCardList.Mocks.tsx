f63ce0de57447fc836839d8b96f35f634c1be9dc
Gregor Aitken <v_gregoraitken@tfl.gov.uk>
2023-11-23 10:14:06 +0000

    added hotel details

--- src/stories/OfferCardList.Mocks.tsx
+++ src/stories/OfferCardList.Mocks.tsx
@@ -1,9 +1,11 @@
 import { type OfferCardPropsI } from "./components/molecules/OfferCard/OfferCard";
 import hotelImage1 from "../../src/stories/assets/hotelImages/hotel-image-1.png";
+import hotelImage2 from "../../src/stories/assets/hotelImages/hotel-image-2.png";
+import hotelImage3 from "../../src/stories/assets/hotelImages/hotel-image-3.png";
 
 export const offerCardListData: OfferCardPropsI[] = [
   {
-    hotelName: "Aguamarina Golf Hotel",
+    hotelName: "Iberostar Grand Salome",
     location: "Cost Adeje, Tenerife",
     starRating: 3,
     hotelImage: hotelImage1,
@@ -16,14 +18,14 @@ export const offerCardListData: OfferCardPropsI[] = [
     price: 6000,
     callToAction: "Book now",
     hotelDescription:
-      "The Hotel Aguamarina Golf has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Golf courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea.",
+      "Located on the coast of Tenerife, between Playa del Duque and Playa de Fanabe, this hotel offers gourmet dining, exclusive lavish spas, magnificent suites with spectacular views and a personal butler or concierge service to meet all of your needs.",
     product: "hotel",
   },
   {
-    hotelName: "Aguamarina Golf Hotel 2",
+    hotelName: "Aguamarina Golf Hotel",
     location: "Cost Adeje, Tenerife",
     starRating: 5,
-    hotelImage: hotelImage1,
+    hotelImage: hotelImage2,
     adults: 2,
     children: 4,
     departureAirport: "Manchester",
@@ -37,10 +39,10 @@ export const offerCardListData: OfferCardPropsI[] = [
     product: "hotel",
   },
   {
-    hotelName: "Aguamarina Golf Hotel 3",
+    hotelName: "Las Piramides Resort",
     location: "Cost Adeje, Tenerife",
     starRating: 1,
-    hotelImage: hotelImage1,
+    hotelImage: hotelImage3,
     adults: 2,
     infants: 0,
     children: 4,
