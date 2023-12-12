4ea89236ecb6cb2a04152e07611bf1b49dd5b31a
Gregor Aitken <gregor.aitken@forwarddigital.co.uk>
2023-12-12 15:02:23 +0000

    update for optimised component

--- src/stories/components/organisms/SortableOfferCards/SortableOfferCards.tsx
+++ src/stories/components/organisms/SortableOfferCards/SortableOfferCards.tsx
@@ -1,8 +1,5 @@
-import React, { useState } from "react";
-import {
-  OfferCard,
-  type OfferCardPropsI,
-} from "../../molecules/OfferCard/OfferCard";
+import React, { useState, useMemo } from "react";
+import { OfferCard, type OfferCardPropsI } from "../../molecules/OfferCard/OfferCard";
 import { SortButton } from "../../atoms/SortButton/SortButton";
 import "./sortableOfferCards.scss";
 import { ReactComponent as Alphabetical } from "../../../assets/alphabetical.svg";
@@ -16,93 +13,56 @@ interface Props {
 }
 
 const SortableInfoCards: React.FC<Props> = ({ data }) => {
-  const [sortedData, setSortedData] = useState<Item[]>(
-    [...data].sort((a, b) => a.price - b.price)
-  );
-  const [activeItem, setActiveItem] = useState<string>(SortListE.PRICE);
-  
-  const handleItemClick = (item: string): void => {
-    setActiveItem(item);
-  };
+  const [activeItem, setActiveItem] = useState<SortListE>(SortListE.PRICE);
+
+  const sortedData = useMemo(() => {
+    const sortData = (data: Item[], option: SortListE) => {
+      return [...data].sort((a, b) => {
+        switch (option) {
+          case SortListE.ALPHABETICAL:
+            return a.hotelName.localeCompare(b.hotelName);
+          case SortListE.PRICE:
+            return a.price - b.price;
+          case SortListE.STAR_RATING:
+            return b.starRating - a.starRating;
+          default:
+            return 0;
+        }
+      });
+    };
+
+    return sortData(data, activeItem);
+  }, [data, activeItem]);
 
   const handleSort = (option: SortListE) => {
-    let sortedArray: Item[] = [];
-
-    switch (option) {
-      case SortListE.ALPHABETICAL:
-        sortedArray = [...data].sort((a, b) =>
-          a.hotelName.localeCompare(b.hotelName)
-        );
-        break;
-      case SortListE.PRICE:
-        sortedArray = [...data].sort((a, b) => a.price - b.price);
-        break;
-      case SortListE.STAR_RATING:
-        sortedArray = [...data].sort((a, b) => b.starRating - a.starRating);
-        break;
-      default:
-        break;
-    }
-    setSortedData(sortedArray);
+    setActiveItem(option);
   };
+
+  const sortingOptions = [
+    { label: "Alphabetical", value: SortListE.ALPHABETICAL, icon: <Alphabetical /> },
+    { label: "Price", value: SortListE.PRICE, icon: <Pound /> },
+    { label: "Star Rating", value: SortListE.STAR_RATING, icon: <Star /> },
+  ];
 
   return (
     <div className="app-container">
       <div className="sortableInfoCards">
         <aside className="sidebar">
-          <SortButton
-            buttonText="sort"
-            isClicked = {activeItem === SortListE.ALPHABETICAL}
-            secondaryBoldText="alphabetically"
-            onClick={() => {
-              handleSort(SortListE.ALPHABETICAL);
-              handleItemClick(SortListE.ALPHABETICAL);
-            }}
-            icon={<Alphabetical />}
-          />
-          <SortButton
-            buttonText="sort by"
-            secondaryBoldText="price"
-            isClicked = {activeItem === SortListE.PRICE}
-            onClick={() => {
-              handleSort(SortListE.PRICE);
-              handleItemClick(SortListE.PRICE);
-            }}
-            icon={<Pound />}
-          />
-          <SortButton
-            buttonText="sort by"
-            secondaryBoldText="star rating"
-            isClicked = {activeItem === SortListE.STAR_RATING}
-            onClick={() => {
-              handleSort(SortListE.STAR_RATING);
-              handleItemClick(SortListE.STAR_RATING);
-            }}
-            icon={<Star />}
-          />
+          {sortingOptions.map(({ label, value, icon }) => (
+            <SortButton
+              key={value}
+              buttonText="sort"
+              isClicked={activeItem === value}
+              secondaryBoldText={label.toLowerCase()}
+              onClick={() => handleSort(value)}
+              icon={icon}
+            />
+          ))}
         </aside>
         <article className="content">
-          {sortedData?.map((item: OfferCardPropsI) => {
-            return (
-              <OfferCard
-                key={item.hotelName}
-                hotelName={item.hotelName}
-                location={item.location}
-                starRating={item.starRating}
-                adults={item.adults}
-                children={item.children}
-                infants={item.infants}
-                departureDate={item.departureDate}
-                days={item.days}
-                departureAirport={item.departureAirport}
-                callToAction={item.callToAction}
-                price={item.price}
-                hotelDescription={item.hotelDescription}
-                product={item.product}
-                hotelImage={item.hotelImage}
-              />
-            );
-          })}
+          {sortedData.map((item: OfferCardPropsI) => (
+            <OfferCard key={item.hotelName} {...item} />
+          ))}
         </article>
       </div>
     </div>
