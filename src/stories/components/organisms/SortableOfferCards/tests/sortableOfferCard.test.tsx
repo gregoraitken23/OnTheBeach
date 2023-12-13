55c5ca165146172e523cec5f736f86762b192fc8
Gregor Aitken <gregor.aitken@forwarddigital.co.uk>
2023-12-13 11:34:55 +0000

    update

--- src/stories/components/organisms/SortableOfferCards/tests/sortableOfferCard.test.tsx
+++ src/stories/components/organisms/SortableOfferCards/tests/sortableOfferCard.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import SortableOffercards from "../SortableOfferCards";
 import { offerCardListData } from "../../../../OfferCardList.Mocks";
 
-test("Does Sortable Offer Cards Load?", () => {
+test("Does Sortable Offer Cards Load", () => {
   const { container } = render(<SortableOffercards data={offerCardListData} />);
   expect(container.childElementCount).toEqual(1);
-});
+}); 
