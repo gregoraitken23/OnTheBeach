b386fd457a86f67cbc2eaccfb7698666d47a17c4
Gregor Aitken <gregor.aitken@forwarddigital.co.uk>
2023-12-13 14:32:12 +0000

    offer card tests added

--- src/stories/components/molecules/OfferCard/tests/offerCard.test.tsx
+++ src/stories/components/molecules/OfferCard/tests/offerCard.test.tsx
@@ -1,28 +1,24 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { OfferCard } from "../OfferCard";
-import { offerCardListData } from "../../../../OfferCardList.Mocks";
+import React from 'react';
+import { render, fireEvent, getByAltText, getByTestId, getByRole, waitFor } from '@testing-library/react';
+import { OfferCard, OfferCardPropsI } from '../OfferCard';
+import { offerCardListData } from '../../../../OfferCardList.Mocks';
 
-test("Does Offer Card Load?", () => {
-  const item = offerCardListData[0];
-  const { container } = render(
-    <OfferCard
-      key={item.hotelName}
-      hotelName={item.hotelName}
-      location={item.location}
-      starRating={item.starRating}
-      adults={item.adults}
-      children={item.children}
-      infants={item.infants}
-      departureDate={item.departureDate}
-      days={item.days}
-      departureAirport={item.departureAirport}
-      callToAction={item.callToAction}
-      price={item.price}
-      hotelDescription={item.hotelDescription}
-      product={item.product}
-      hotelImage={item.hotelImage}
-    />
-  );
-  expect(container.childElementCount).toEqual(1);
+const data = offerCardListData[0];
+describe('OfferCard Component loads', () => {
+  it('renders without crashing', () => {
+    render(<OfferCard {...data} />);
+  });
+  it('displays hotel details correctly', () => {
+    const { getByText } = render(<OfferCard {...data} />);
+    expect(getByText(data.hotelName)).toBeInTheDocument();
+    expect(getByText(data.location)).toBeInTheDocument();
+    expect(getByText(data.callToAction)).toBeInTheDocument();
+    // Add similar assertions for other details
+  });
+  it('displays the hotel image correctly', () => {
+    const { getByAltText } = render(<OfferCard {...data} />);
+    const image = getByAltText(data.hotelName);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', data.hotelImage);
+  });
 });
